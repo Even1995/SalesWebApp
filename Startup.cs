b25e96cbac029f63e36b35ee7a0ade8a97555412
Even1995 <even.leah@gmail.com>
2022-01-05 08:37:34 -0300

    first migration

--- Startup.cs
+++ Startup.cs
@@ -36,8 +36,10 @@ namespace SalesWebApp
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<SalesWebAppContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebAppContext")));
+           
+            services.AddDbContext< SalesWebAppContext> (options =>
+             options.UseMySql(Configuration.GetConnectionString("SalesWebAppContext"), builder =>
+                builder.MigrationsAssembly("SalesWebApp")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
