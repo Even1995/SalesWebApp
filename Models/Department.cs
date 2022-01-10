f2ee822acba0306e9323ea1ee4b6bccdb24ccfda
Even1995 <even.leah@gmail.com>
2022-01-09 21:11:12 -0300

    segunda migration

--- Models/Department.cs
+++ Models/Department.cs
@@ -8,6 +8,25 @@ namespace SalesWebApp.Models
     public class Department
     {
         public string Name { get; set; }
-            public int Id { get; set; }
+        public int Id { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public void AddSeller(Seller se)
+        {
+            Sellers.Add(se);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(Seller => Seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ Models/SaleRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebApp.Models
+{
+    public class SaleRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public double Amount { get; set; }
+
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SaleRecord()
+        {
+
+        }
+        public SaleRecord(int id, DateTime date, double amount, SaleStatus status, Seller Seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            this.Seller = Seller;
+        }
+    }
+}
