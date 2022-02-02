c96d5db5cc3584c2ca453915a21c9412f118e8ba
Even1995 <even.leah@gmail.com>
2022-02-02 08:06:54 -0300

    Page Create a new Seller

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebApp.Models;
+using SalesWebApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,28 @@ namespace SalesWebApp.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService;
+        
+        public SellersController (SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
         public IActionResult Index()
+        {
+            var list = _sellerService.FindAllAsync();
+            return View(list);
+        }
+        public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.InsertAsync(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
